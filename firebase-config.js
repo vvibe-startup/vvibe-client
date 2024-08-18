661ca10f893cc9e8a9230514c8368b6d8297d3bf
Nam Nguyen <nam.nguy3019@gmail.com>
2024-08-17 20:04:20 -0500

    feat: add firebase config file

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBeCourPkbbx65WpmsY6M4lYU07owGuYDg',
+  authDomain: 'vvibe-e3f56.firebaseapp.com',
+  projectId: 'vvibe-e3f56',
+  storageBucket: 'vvibe-e3f56.appspot.com',
+  messagingSenderId: '36392085114',
+  appId: '1:36392085114:web:d8e24f57131c2e5849fdc3',
+  measurementId: 'G-9VR1V1HW0P',
+};
+const app = initializeApp(firebaseConfig);
+export default app;
